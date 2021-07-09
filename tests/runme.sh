8f69909c5fe9cb1b5178796acd345d3c7736c6c0
Michele Gandolfi <michelegandolfi93@gmail.com>
2021-07-09 12:05:36 +0200

    locate_neurons moved to locate_NGas

--- tests/runme.sh
+++ tests/runme.sh
@@ -5,19 +5,20 @@
 # Normal modes coordinates
 echo "******NGas FROM NORMAL MODES******"
 echo 
-../bin/locate_neurons.py nm_water/h2o_NMq.dat -N 100 -O nm_water/neurons -T -i 0:1000
+../bin/locate_NGas.py nm_water/h2o_NMq.dat -N 100 -O nm_water/neurons -T -i 0:1000
 echo
-echo "Converting neurons to Cartesian. Output in cart_water/neurons_traj-cart.xyz"
-echo 
+echo "Converting neurons to Cartesian.."
+# Output in cart_water/neurons_traj-cart.xyz"
 
 ../bin/toCart.py nm_water/neurons-traj.dat  nm_water/cnorm.dat  nm_water/mass.dat  nm_water/qrt.dat  cart_water/neurons_traj-cart.xyz
 
+echo 
 echo "******DBH FROM NORMAL MODES******"
 echo 
 ../bin/locate_DBq.py nm_water/h2o_NMq.dat -R 2.98 -O nm_water/dbq -T -i 0:1000
 echo
-echo "Converting neurons to Cartesian. Output in cart_water/dbq_traj-cart.xyz"
-echo 
+echo "Converting neurons to Cartesian.."
+# Output in cart_water/dbq_traj-cart.xyz"
 
 ../bin/toCart.py  nm_water/dbq-traj.dat  nm_water/cnorm.dat  nm_water/mass.dat  nm_water/qrt.dat  cart_water/dbq_traj-cart.xyz
 
@@ -25,7 +26,7 @@ echo
 echo
 echo "******NGas FROM CARTESIAN (FOR TESTING PURPOSES ONLY)******"
 echo
-../bin/locate_neurons.py cart_water/h2o_traj.xyz --xyz -S cent -N 100 -O cart_water/neurons-cart -T
+../bin/locate_NGas.py cart_water/h2o_traj.xyz --xyz -S cent -N 100 -O cart_water/neurons-cart -T
 
 echo 
 echo "You may compare cart_water/neurons_traj-cart.xyz, cart_water/dbq_traj-cart.xyz and cart_water/h2o_traj.xyz"
